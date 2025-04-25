ca2edb09f22ba34d3da11b6b04c32f31654075df
xxxCoeNxxx <pedrocela1992@gmail.com>
2025-04-25 19:23:22 +0200

    no me sale el ej1

--- src/main/scala/Listas.scala
+++ src/main/scala/Listas.scala
@@ -3,14 +3,19 @@ import scala.collection.mutable.Map
 
 object Listas {
     def main (args: Array[String]): Unit = {
-        
+        ejercicio1()
         ejercicio2()
         ejercicio3()
     }
 
 
-        // Ejercicio 1: Elimina duplicados y cuenta ocurrencias
-        //val palabras = List("gato", "perro", "gato", "loro", "perro", "gato")
+    // Ejercicio 1: Elimina duplicados y cuenta ocurrencias
+    //val palabras = List("gato", "perro", "gato", "loro", "perro", "gato")
+    private def ejercicio1(): Unit = {
+        val palabras = List("gato", "perro", "gato", "loro", "perro", "gato")
+
+
+    }
 
 
     //Ejercicio 2: Encuentra el segundo mayor
@@ -33,47 +38,3 @@ object Listas {
         println(numerosFiltrados)
     }
 }
-
-
-
-
-
-
-/* Ejercicio 1: Elimina duplicados y cuenta ocurrencias
-Dada una lista de palabras, elimina las palabras repetidas y muestra cuántas veces aparecía cada una en la lista original.
-
-Ejemplo:
-
-val palabras = List("gato", "perro", "gato", "loro", "perro", "gato")
-Salida esperada:
-
-text
-gato: 3
-perro: 2
-loro: 1
-
-
-
-Ejercicio 2: Encuentra el segundo mayor
-Dada una lista de enteros, encuentra el segundo número más grande de la lista. Si no existe, muestra un mensaje adecuado.
-
-Ejemplo:
-
-val numeros = List(5, 3, 9, 1, 9, 7)
-Salida esperada:
-
-El segundo mayor es: 7
-
-
-
-Ejercicio 3: Filtra y transforma
-Dada una lista de números, filtra los que sean múltiplos de 3 y mayores que 10, y luego crea una nueva lista con el 
-doble de esos valores.
-
-Ejemplo:
-
-val numeros = List(3, 6, 12, 18, 20, 21, 9)
-Salida esperada:
-
-List(24, 36, 42)
-Porque 12, 18 y 21 cumplen la condición, y su doble es 24, 36, 42 */
